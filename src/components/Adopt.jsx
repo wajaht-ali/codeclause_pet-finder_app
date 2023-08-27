93b91baf98a6b1768b650ab18338f71031edc2f1
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-08-27 08:54:35 +0500

    contact page updated successfully

--- src/components/Adopt.jsx
+++ src/components/Adopt.jsx
@@ -1,35 +1,41 @@
 import React from 'react'
 import "../styling/Adopt.scss";
 import data from "../assets/database.json";
+import Footer from './Footer';
 
 const Adopt = () => {
   return (
-    <div className='adoption'>
-      <div className="headings">
-        <h1>Find Your Pet for Adoption here: </h1>
-        <div className="btns">
-          <button>Dogs</button>
-          <button>Cats</button>
-          <button>Rabbits</button>
-          <button>Fishes</button>
+    <div className='main'>
+      <div className='adoption'>
+        <div className="headings">
+          <h1>Find Your Pet for Adoption here: </h1>
+          <div className="btns">
+            <button>Dogs</button>
+            <button>Cats</button>
+            <button>Rabbits</button>
+            <button>Fishes</button>
+          </div>
         </div>
+        <div className="cards">
+
+          {
+            data.adoptions.map(i => (
+              <div className='card'>
+                <img src={i.img} alt={i.firstname} />
+                <h2>{i.firstname}</h2>
+                <p>{i.about}</p>
+                <span>
+                  <h3>Type: {i.category}</h3>
+                  <button>Adopt!</button>
+                </span>
+              </div>
+            ))
+          }
+
+        </div>
+      <div className="footer">
+        <Footer />
       </div>
-      <div className="cards">
-
-        {
-          data.adoptions.map(i => (
-            <div className='card'>
-              <img src={i.img} alt={i.firstname} />
-              <h2>{i.firstname}</h2>
-              <p>{i.about}</p>
-              <span>
-                <h3>Type: {i.category}</h3>
-                <button>Adopt!</button>
-              </span>
-            </div>
-          ))
-        }
-
       </div>
     </div>
   )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { Toaster, toast } from "react-hot-toast";
+import { MdSend } from "react-icons/md";
+import "../styling/Contact.scss";
+import Footer from './Footer';
 
 const Contact = () => {
+
+  const notify = () => {
+    toast("Response Submitted Successfully!",  {
+      duration: 4000,
+      icon: '👏',
+      position: "top-center"
+    })
+  }
+
+
   return (
-    <div>
-      Contact
+    <div >
+      <div className="contact-page">
+        <div className="heading">
+          <h1>Contact us for any kind of query!</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque, necessitatibus.</p>
+        </div>
+        <div className="contact-form">
+          <form action="">
+
+            <div className="form-items">
+              <label htmlFor="name">Name: </label>
+              <input type="text" id='name' placeholder='Your name...' required />
+              <label htmlFor="email">Email: </label>
+              <input type="email" id='name' placeholder='Your email...' required />
+              <label htmlFor="message">Message: </label>
+              <textarea cols="30" id='message' rows="10" placeholder='Your Message...'></textarea>
+              <button onClick={notify}><MdSend /></button>
+              <Toaster />
+            </div>
+
+          </form>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
